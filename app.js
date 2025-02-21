4c04b49e5ec2ac9d66a0b866666710f81329d92a
anoint7483 <yabhishek8774@gmail.com>
2025-02-21 13:45:37 +0530

    changes in some logic and css

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 let userScore = 0;
-let comScore = 0;
+let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 const msg = document.querySelector(".msg"); // Use correct selector for the message element
+const user_score = document.getElementById("user-score");
+const comp_score = document.getElementById("comp-score");
+
 
 const genCompChoice = () => {
     const options = ["rock", "paper", "scissors"];
@@ -13,15 +16,22 @@ const genCompChoice = () => {
 const drawGame = () => {
     console.log("game was draw.");
     msg.innerText = 'Game was a draw. Play again!';
+    msg.style.color = "yellow";
 }
 
 const showWinner = (userWin) => {
     if (userWin) {
         console.log("you win!");
         msg.innerText = "You win!";
+        msg.style.color = "green";
+        userScore ++;
+        user_score.innerText = userScore
     } else {
         console.log("you lose");
         msg.innerText = "You lose.";
+        msg.style.color = "red";
+        compScore++;
+        comp_score.innerText = compScore
     }
 }
 
@@ -36,12 +46,25 @@ const playGame = (userChoice) => {
     } else {
         let userWin = true;
         if (userChoice === "rock") {
-            userWin = compChoice === "paper" ? false : true;
+            if (compChoice === "paper") {
+                userWin = false;
+            } else {
+                userWin = true;
+            }
         } else if (userChoice === "paper") {
-            userWin = compChoice === "scissors" ? false : true;
+            if (compChoice === "scissors") {
+                userWin = false;
+            } else {
+                userWin = true;
+            }
         } else {
-            userWin = compChoice === "rock" ? false : true;
+            if (compChoice === "rock") {
+                userWin = false;
+            } else {
+                userWin = true;
+            }
         }
+        
 
         showWinner(userWin);
     }
@@ -50,7 +73,7 @@ const playGame = (userChoice) => {
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
         // Get user choice by its id (rock, paper, or scissors)
-        const userChoice = choice.getAttribute("class");
+        const userChoice = choice.getAttribute("id");
         playGame(userChoice);
     });
 });
